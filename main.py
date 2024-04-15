7a6ad656ec1c51a65a6c34d02a7c1a0852e0adea
Bogdan <94449180+kruglovbogL@users.noreply.github.com>
2024-04-15 15:37:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import requests
+import json
+import time
+import os
+import pandas as pd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import normalize
+from sklearn.decomposition import PCA
+
+def get_vacancies(keyword):
+    url = "https://api.hh.ru/vacancies"
+    params = {
+        "text": keyword,
+        "area": 1,  # Specify the desired area ID (1 is Moscow)
+        "per_page": 30,  # Number of vacancies per page
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",  # Replace with your User-Agent header
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        vacancies = data.get("items", [])
+        for vacancy in vacancies:
+            # Extract relevant information from the vacancy object
+            vacancy_id = vacancy.get("id")
+            vacancy_job = vacancy.get('schedule', {}).get("name")
+            vacancy_title = vacancy.get("name")
+            vacancy_url = vacancy.get("alternate_url")
+            company_name = vacancy.get("employer", {}).get("name")
+            print(f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\nJob: {vacancy_job}\n")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+# Example usage
+get_vacancies("python developer junior")
+
+
