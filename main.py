d992cff1f6612ea80d77a46ce4c910011a3cee28
kruglovbog <kruglovbog@gmail.com>
2024-04-19 00:06:13 +0300

    regedit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: \xe7
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
@@ -5,26 +7,26 @@ import seaborn as sns
 
 
 def get_vacancies(keyword):
-    url = "https://api.hh.ru/vacancies"  # загрузка с помощью api
+    url = "https://api.hh.ru/vacancies"  # ???????? ? ??????? api
     params = {
         "text": keyword,
-        "area": 1,  # Если 1 то Москва или поиск пор всем районам
-        "per_page": 50,  # Количество страниц для поиска
+        "area": 1,  # ???? 1 ?? ?????? ??? ????? ??? ???? ???????
+        "per_page": 50,  # ?????????? ??????? ??? ??????
     }
-    headers = {  # голова запроса, без нее запрос не выполниться
+    headers = {  # ?????? ???????, ??? ??? ?????? ?? ???????????
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/123.0.0.0 Safari/537.36",  # Заменить на свой браузер
+                      "Chrome/123.0.0.0 Safari/537.36",  # ???????? ?? ???? ???????
 
     }
 
     response = requests.get(url, params=params, headers=headers)
 
-    data_frame = pd.DataFrame()  # создание DataFrame с данными
-    if response.status_code == 200:  # если статус код 200 то
-        data = response.json()  # получить json
-        vacancies = data.get("items", [])  # получить items
+    data_frame = pd.DataFrame()  # ???????? DataFrame ? ???????
+    if response.status_code == 200:  # ???? ?????? ??? 200 ??
+        data = response.json()  # ???????? json
+        vacancies = data.get("items", [])  # ???????? items
         for vacancy in vacancies:
-            # Информация о вакансиях id, job, title, url, name
+            # ?????????? ? ????????? id, job, title, url, name
             vacancy_id = vacancy.get("id")
             vacancy_job = vacancy.get('schedule', {}).get("name")
             vacancy_title = vacancy.get("name")
@@ -33,19 +35,19 @@ def get_vacancies(keyword):
             print(
                 f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\nJob: {vacancy_job}\n")
             data_frame = data_frame._append(vacancy, ignore_index=True)
-            data_frame.to_csv('list_vacancies.csv', index=False)  # создание файла с данными
+            data_frame.to_csv('list_vacancies.csv', index=False)  # ???????? ????? ? ???????
     else:
-        print(f"Request failed with status code: {response.status_code}")  # другой статус код НЕ 200.
+        print(f"Request failed with status code: {response.status_code}")  # ?????? ?????? ??? ?? 200.
 
 
-get_vacancies("python developer junior")  # запрос текста INPUT
+get_vacancies("python developer junior")  # ?????? ?????? INPUT
 
-X = pd.read_csv('list_vacancies.csv')  # чтение csv файла
+X = pd.read_csv('list_vacancies.csv')  # ?????? csv ?????
 X.describe()
 print(X.columns)
 new_data = X.dropna()
 
 fig = plt.figure
 fig, ax = plt.subplots(figsize=(7, 7))
-sns.distplot(X.id, color='red', label='bmi', ax=ax)  # seaborn график по id вакансий, запихнуть любые ЧИСЛОВЫЕ данные
+sns.distplot(X.id, color='red', label='bmi', ax=ax)  # seaborn ?????? ?? id ????????, ????????? ????? ???????? ??????
 plt.show()
