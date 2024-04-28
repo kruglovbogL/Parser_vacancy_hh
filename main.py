dc384c7fef782e7c4210bb161556e6de77a4df9e
Bogdan <94449180+kruglovbogL@users.noreply.github.com>
2024-04-29 00:42:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,87 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8-bom
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import normalize
-from sklearn.decomposition import PCA
+import seaborn as sns
 
 def get_vacancies(keyword):
-    url = "https://api.hh.ru/vacancies"
+    url = "https://api.hh.ru/vacancies"  # загрузка с помощью api
     params = {
         "text": keyword,
-        "area": 1,  # Specify the desired area ID (1 is Moscow)
-        "per_page": 50,  # Number of vacancies per page
+        "area": 1,  # Если 1 то Москва или поиск пор всем районам
+        "per_page": 50,  # Количество страниц для поиска
     }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",  # Replace with your User-Agent header
+    headers = {  # голова запроса, без нее запрос не выполниться
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/123.0.0.0 Safari/537.36",  # Заменить на свой браузер
+
     }
 
     response = requests.get(url, params=params, headers=headers)
 
-    # Creating a DataFrame
-    df = pd.DataFrame()
-    if response.status_code == 200:
-        data = response.json()
-        vacancies = data.get("items", [])
+    data_frame = pd.DataFrame()  # создание DataFrame с данными
+    if response.status_code == 200:  # если статус код 200 то
+        data = response.json()  # получить json
+        vacancies = data.get("items", [])  # получить items
         for vacancy in vacancies:
-            # Extract relevant information from the vacancy object
+            # Информация о вакансиях id, job, title, url, name
+            vacancy_cash = vacancy.get("salary", {}.get("from"))
             vacancy_id = vacancy.get("id")
-            vacancy_job = vacancy.get('schedule', {}).get("name")
+            vacancy_job = vacancy.get("schedule", {}).get("name")
             vacancy_title = vacancy.get("name")
             vacancy_url = vacancy.get("alternate_url")
             company_name = vacancy.get("employer", {}).get("name")
-            print(f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\nJob: {vacancy_job}\n")
-            df = df._append(vacancy, ignore_index=True)
-            df.to_csv('movie_example1.csv', index=False)
+            print(
+                f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\nJob: {vacancy_job}\nCash: {vacancy_cash}\n")
+            data_frame = data_frame._append(vacancy, ignore_index=True)
+            data_frame.to_csv('list_vacancies.csv', index=False)  # создание файла с данными
     else:
-        print(f"Request failed with status code: {response.status_code}")
-# Example usage
-get_vacancies("python developer junior")
+        print(f"Request failed with status code: {response.status_code}")  # другой статус код НЕ 200.
 
-X = pd.read_csv('movie_example1.csv')
 
-resylt = X['id'].tolist()
-print(resylt)
-colums = X.columns.tolist()
-print(colums)
-resuly = X[['name', 'schedule', 'url', 'employer', 'id']]
-df = pd.DataFrame(resuly)
+get_vacancies("python developer junior")  # запрос текста INPUT
 
-df.id.unique()
-df.plot()
+X = pd.read_csv('list_vacancies.csv')  # чтение csv файла c таблицей данных!
+X.describe()
+print(X.columns)
+new_data = X.dropna()
+
+fig = plt.figure
+fig, ax = plt.subplots(figsize=(7, 7))
+sns.distplot(X.id, color='red', label='bmi',
+             ax=ax)  # seaborn график по id вакансий, запихнуть любые ЧИСЛОВЫЕ данные DATAFRAME
 plt.show()
-
-
-#
-# # Numpy array of all the cluster labels assigned to each data point
-# db_default = DBSCAN(eps = 0.0375, min_samples = 3).fit(X_principal)
-# labels = db_default.labels_
-# #-----------------------------------------------------------------
-# # Building the label to colour mapping
-# colours = {}
-# colours[0] = 'r'
-# colours[1] = 'g'
-# colours[2] = 'b'
-# colours[-1] = 'k'
-#
-# # Building the colour vector for each data point
-# cvec = [colours[label] for label in labels]
-#
-# # For the construction of the legend of the plot
-# r = plt.scatter(X_principal['P1'], X_principal['P2'], color ='r');
-# g = plt.scatter(X_principal['P1'], X_principal['P2'], color ='g');
-# b = plt.scatter(X_principal['P1'], X_principal['P2'], color ='b');
-# k = plt.scatter(X_principal['P1'], X_principal['P2'], color ='k');
-#
-# # Plotting P1 on the X-Axis and P2 on the Y-Axis
-# # according to the colour vector defined
-# plt.figure(figsize =(9, 9))
-# plt.scatter(X_principal['P1'], X_principal['P2'], c = cvec)
-#
-# # Building the legend
-# plt.legend((r, g, b, k), ('Label 0', 'Label 1', 'Label 2', 'Label -1'))
-#
-# plt.show()
